2469547ae7adde99390bc9d934c89208d58f4478
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-10-19 14:21:18 -0400

    simple portion almost complete

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,6 +4,10 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+
+    String bob = "Name is bob";
+    Integer jeff = 3;
+
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
@@ -13,5 +17,28 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
+        Console.println(addNum(5, 6).toString());
+
     }
+
+    public static Integer addNum(Integer a, Integer b){
+        return a + b;
+    }
+
+    public static Integer subNum(Integer a, Integer b){
+        return a - b;
+    }
+
+    public static Integer multiNum(Integer a, Integer b){
+        return a * b;
+    }
+
+    public static Integer divNum(Integer a, Integer b){
+        return a / b;
+    }
+
+
+
+
 }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,27 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.Test;
+
+import static com.zipcodewilmington.scientificcalculator.MainApplication.addNum;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+    @Test
+    public void addNumTest(){
+        assertEquals(Integer.valueOf(2), addNum(1, 1));
+    }
+
+    @Test
+    public void subNumTest(){
+
+        assertEquals(Integer.valueOf(1), subNum(3, 2);
+    }
+
+    @Test
+    public void multiNum()
 }

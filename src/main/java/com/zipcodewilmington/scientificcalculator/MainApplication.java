075a0540c4bed0fb9e22e629b2a8157e4ee4b7be
Kievina <56592501+Kievina@users.noreply.github.com>
2019-10-20 20:36:44 -0400

    lastest MainApplication

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -2,6 +2,8 @@ package com.zipcodewilmington.scientificcalculator;
 
 //import sun.java2d.pipe.SpanShapeRenderer;
 
+//import java.util.Scanner;
+
 /**
  * Created by leon on 2/9/18.
  */
@@ -10,75 +12,86 @@ public class MainApplication {
 
     public static void main(String[] args) {
         Double result;
+        Double value;
+        Double value1;
+        Double value2;
+        int mode = 0;
         SimpleCalculator simpleCalc = new SimpleCalculator();
         Scientific scientificCalc = new Scientific();
+
+
         Console.println("Welcome to this Damn calculator!");
-        Integer mode = 0;
 
         while (mode == 0) {
-            mode = Console.getIntegerInput("Choose the mode number: \n" + "1 - Simple Calculator \n" + "2 - Scientific Calculator");
 
-            while (mode == 1) {
+            mode = Console.getIntegerInput("Choose mode by number: \n"
+                    + "1 - Simple Calculator \n"
+                    + "2 - Scientific Calculator");
 
-                Integer operator = Console.getIntegerInput("Choose your operation by number: \n" +
-                        "1 - Addition \n" + "2 - Subtraction \n" + "3 - Multiplication \n" + "4 - Division \n" + "5 - Exit to Main Menu");
-                if (operator == 5) {
-                    mode = 0;
-                    break;
-                }
-                Double value1 = Console.getDoubleInput("Enter the first value.");
-                Double value2 = Console.getDoubleInput("Enter the second value.");
-
-
-                switch(operator)
-                {
-                    case 1:
-                        result = simpleCalc.addNum(value1, value2);
-                        Console.println("%f + %f = %f", value1, value2, result);
-                        break;
-                    case 2:
-                        result = simpleCalc.subNum(value1, value2);
-                        Console.println("%f - %f = %f", value1, value2, result);
-                        break;
-                    case 3:
-                        result = simpleCalc.multiNum(value1, value2);
-                        Console.println("%f * %f = %f", value1, value2, result);
-                        break;
-                    case 4:
-                        result = simpleCalc.divNum(value1, value2);
-                        Console.println("%f / %f = %f", value1, value2, result);
-                        break;
-                    case 5:
-                        mode = 0;
-                        break;
-                    default:
-                        System.out.println("Err: Invalid Entry");
-                        break;
-                }
+            if (mode != 1 & mode != 2) {
+                System.out.println("Err: Invalid Entry");
                 mode = 0;
+            } else {
+                while (mode == 1) {
 
+                    Integer operator = Console.getIntegerInput("Choose operation by number: \n"
+                            + "1 - Addition \n" + "2 - Subtraction \n"
+                            + "3 - Multiplication \n" + "4 - Division \n"
+                            + "5 - Exit to Main Menu");
+
+                    switch (operator) {
+                        case 1:
+                            value1 = Console.getDoubleInput("Enter the first value.");
+                            value2 = Console.getDoubleInput("Enter the second value.");
+                            result = simpleCalc.addNum(value1, value2);
+                            Console.println("%f + %f = %f", value1, value2, result);
+                            break;
+                        case 2:
+                            value1 = Console.getDoubleInput("Enter the first value.");
+                            value2 = Console.getDoubleInput("Enter the second value.");
+                            result = simpleCalc.subNum(value1, value2);
+                            Console.println("%f - %f = %f", value1, value2, result);
+                            break;
+                        case 3:
+                            value1 = Console.getDoubleInput("Enter the first value.");
+                            value2 = Console.getDoubleInput("Enter the second value.");
+                            result = simpleCalc.multiNum(value1, value2);
+                            Console.println("%f * %f = %f", value1, value2, result);
+                            break;
+                        case 4:
+                            value1 = Console.getDoubleInput("Enter the first value.");
+                            value2 = Console.getDoubleInput("Enter the second value.");
+                            result = simpleCalc.divNum(value1, value2);
+                            Console.println("%f / %f = %f", value1, value2, result);
+                            break;
+                        case 5:
+                            mode = 0;
+                            break;
+                        default:
+                            System.out.println("Err: Invalid Entry");
+                            mode = 1;
+                            break;
+                    }
+                }
             }
 
             while (mode == 2) {
-                Integer operatorSci = Console.getIntegerInput("Choose your operation: \n" +
+                Integer operatorSci = Console.getIntegerInput("Choose operation by number: \n" +
                         "1 - Inverse \n" + "2 - Square \n" + "3 - Square root \n" + "4 - Exit to main menu");
-
-                if (operatorSci == 4) {
-                    mode = 0;
-                    break;
-                }
-                Double value = Console.getDoubleInput("Enter your value.");
 
                 switch (operatorSci) {
                     case 1:
+                        value = Console.getDoubleInput("Enter your value.");
                         result = scientificCalc.findInverse(value);
                         Console.println("%f", result);
                         break;
                     case 2:
+                        value = Console.getDoubleInput("Enter your value.");
                         result = scientificCalc.findSquare(value);
                         Console.println("%f", result);
                         break;
                     case 3:
+                        value = Console.getDoubleInput("Enter your value.");
                         result = scientificCalc.findSquareRoot(value);
                         Console.println("%f", result);
                         break;
@@ -86,15 +99,17 @@ public class MainApplication {
                         mode = 0;
                         break;
                     default:
-                        //System.out.println("Err: Invalid Entry");
+                        System.out.println("Err: Invalid Entry");
+                        mode = 2;
                         break;
                 }
-                mode = 0;
             }
         }
-
     }
 }
+
+
+
 
 
 
